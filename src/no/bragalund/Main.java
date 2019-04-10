86d4339d0235577e5ae599f00ffc62ad909b013b
bragalund <bakkelund_91@hotmail.com>
2019-04-10 11:51:21 +0200

    Initial commit.

---
+++ src/no/bragalund/Main.java
@@ -0,0 +1,17 @@
+package no.bragalund;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        if(args[0].equals("1")){
+
+        }else if(args[1].equals("2")){
+
+        }else if(args[2].equals("3")){
+
+        }else{
+            System.out.println("Supplied wrong input! Pleas choose 1,2 or 3");
+        }
+    }
+}
