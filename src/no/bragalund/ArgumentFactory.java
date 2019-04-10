a804ff132a8e1463ffab71d0b2b97f9cb5cebed2
bragalund <bakkelund_91@hotmail.com>
2019-04-10 15:44:36 +0200

    Removed throwing of illegalexception several places.

--- src/no/bragalund/ArgumentFactory.java
+++ src/no/bragalund/ArgumentFactory.java
@@ -1,19 +1,30 @@
 package no.bragalund;
 
 public class ArgumentFactory {
-    public AbstractArgument createArgumentBasedOnInput(String input){
-        switch (input){
-            case "1":
-                return new NumberOneArgument();
-            case "2":
-                return new NumberTwoArgument();
-            case "3":
-                return new NumberThreeArgument();
-            default:
-                System.out.println("Supplied wrong input! Please choose 1,2 or 3");
-                break;
+    public AbstractArgument createArgumentBasedOnInput(String[] args){
+
+        if(inputArgsIsValid(args)){
+            String input = args[0];
+            switch (input){
+                case "1":
+                    return new NumberOneArgument();
+                case "2":
+                    return new NumberTwoArgument();
+                case "3":
+                    return new NumberThreeArgument();
+                default:
+                    System.out.println("Supplied wrong input! Please choose 1,2 or 3");
+                    break;
+            }
         }
-    throw new IllegalArgumentException("Input was wrong!");
+
+        throw new IllegalArgumentException("You should pass in 1, 2 or 3 as arguments!");
+    }
+
+    private boolean inputArgsIsValid(String[] args){
+        if(args == null || args.length < 1){
+ return false;       }
+       return true;
     }
 
 }
--- src/no/bragalund/Main.java
+++ src/no/bragalund/Main.java
@@ -8,7 +8,7 @@ public class Main {
             throw new IllegalArgumentException("You should pass in 1, 2 or 3 as arguments!");
         }
         ArgumentFactory argumentFactory = new ArgumentFactory();
-        AbstractArgument argument = argumentFactory.createArgumentBasedOnInput(args[0]);
+        AbstractArgument argument = argumentFactory.createArgumentBasedOnInput(args);
         argument.writeCommandLineArgument();
     }
 }
